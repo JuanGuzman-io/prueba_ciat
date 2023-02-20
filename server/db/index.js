58fc623c0b66e8ad945db8aba22f7e9ceb100f5a
Juan D. Guzmán <56206103+JuanGuzman-io@users.noreply.github.com>
2023-02-19 20:56:41 -0500

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE
+    database: process.env.MYSQLDATABASE,
     port: process.env.MYSQLPORT
 });
 
